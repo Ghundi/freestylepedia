7f6d3b1d468fa8121f207aa75900f83893e79956
rjansked <rjansked@students.uni-mainz.de>
2024-12-02 12:41:52 +0100

    added guardians to supporters added brush tutorial from guardians

--- src/scripts/store.js
+++ src/scripts/store.js
@@ -8,6 +8,7 @@ import MunichEast from "../assets/supporters/RZ_freestyler_Logo_1-2.webp";
 import Turtle from "../assets/supporters/Turtle_upscaled.webp"
 import NK from "../assets/supporters/nagyerdei korisok.webp"
 import IceRad from "../assets/supporters/ICERAD.webp"
+import Guardians from "../assets/supporters/Guardians.webp"
 
 export const useFAQ = defineStore('FAQ', {
     state: () => {
@@ -35,6 +36,10 @@ export const useSupporters = defineStore('supporters', {
                 {
                     imgSrc: GlobalIce,
                     link: 'https://www.instagram.com/globaliceskate'
+                },
+                {
+                    imgSrc: Guardians,
+                    link: 'https://www.youtube.com/@icefreestyleguardians'
                 },
                 {
                     imgSrc: MunichEast,
