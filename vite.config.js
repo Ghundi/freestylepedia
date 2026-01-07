d027fb45c45e58f1ce8b0c0e1b7b551570097463
raoul <jdjunior20@gmail.com>
2026-01-07 22:22:19 +0100

    bumped node to 20.19 imported PWA in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 import vuetify from 'vite-plugin-vuetify'
 
